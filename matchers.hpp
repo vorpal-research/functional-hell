314e57cf0f264d150d7bffe40ddda9c15a166087
Mikhail Belyaev <devnull@localhost>
2013-12-03 19:55:47 +0400

    Made unappliers non-static

--- matchers.hpp
+++ matchers.hpp
@@ -8,6 +8,8 @@
 #ifndef MATCHERS_HPP_
 #define MATCHERS_HPP_
 
+#include <tuple>
+
 #include "matchers/match_tuple.hpp"
 #include "matchers/int_type_map.hpp"
 
@@ -35,15 +37,15 @@ struct applicate< match_result<> > {
 template<size_t S, class Storage, class MatchRes, class ...Args>
 struct unapplier {
 	static constexpr size_t K = sizeof...(Args) - S;
-	static bool doIt(Storage& storage, MatchRes&& mres) {
-		if(!type_at_t<K, match_result<Args...>>::template unapply_impl(storage, get<K>(mres))) return false;
-		return unapplier<S-1, Storage, MatchRes, Args...>::doIt(storage, std::forward<MatchRes>(mres));
+	static bool doIt(const std::tuple<Args...>& matchers, Storage& storage, MatchRes&& mres) {
+		if(!std::get<K>(matchers).unapply_impl(storage, get<K>(mres))) return false;
+		return unapplier<S-1, Storage, MatchRes, Args...>::doIt(matchers, storage, std::forward<MatchRes>(mres));
 	}
 };
 
 template<class Storage, class MatchRes, class ...Args>
 struct unapplier<0, Storage, MatchRes, Args...> {
-	static bool doIt(Storage&, MatchRes&&) {
+	static bool doIt(const std::tuple<Args...>& matchers, Storage&, MatchRes&&) {
 		return true;
 	}
 };
@@ -76,21 +78,59 @@ T& unwrap(match_reference<T> ref) {
 	return ref.get();
 }
 
-template<class Lam, class ...Args>
-struct matcher {
+struct matcher {};
 
-	template<class V>
-	using unapplied = decltype(Lam::unapply(std::forward<lower_ref_t<V>>(std::declval<lower_ref_t<V>>())));
+template<class H>
+struct constant_matcher: matcher {
+	template<class V> using elements = nil_map;
 
-	template<class V>
-	using elements = typename applicate< unapplied<V>, Args... >::type;
+	wrap_t<H> data;
+
+	constant_matcher(H h): data(h) {};
 
 	template<class T, class V>
-	static bool unapply_impl(T& storage, V&& v) {
-		auto ret = Lam :: unapply(unwrap(v));
+	bool unapply_impl(T&, V&& value) const{
+		return data == std::forward<V>(value);
+	}
+};
+
+template<class T, class = void>
+struct toMatcher;
+
+template<class T>
+struct toMatcher<T, typename std::enable_if<std::is_base_of<matcher, T>::value>::type> {
+	using type = T;
+};
+
+template<class T>
+struct toMatcher<T, typename std::enable_if<!std::is_base_of<matcher, T>::value>::type> {
+	using type = constant_matcher<T>;
+};
+
+template<class T>
+using toMatcher_t = typename toMatcher<T>::type;
+
+
+template<class Lam, class ...Args>
+struct tree_matcher: matcher {
+
+	Lam lam;
+	std::tuple<toMatcher_t<Args>...> matchers;
+
+	tree_matcher(Lam lam, Args... args): lam(lam), matchers{ args... } {}
+
+	template<class V>
+	using unapplied = decltype(lam.unapply(std::forward<lower_ref_t<V>>(std::declval<lower_ref_t<V>>())));
+
+	template<class V>
+	using elements = typename applicate< unapplied<V>, toMatcher_t<Args>... >::type;
+
+	template<class T, class V>
+	bool unapply_impl(T& storage, V&& v) const {
+		auto ret = lam.unapply(unwrap(v));
 		if(!ret) return false;
 
-		return unapplier<sizeof...(Args), T, decltype(ret), Args...>::doIt(storage, std::move(ret));
+		return unapplier<sizeof...(Args), T, decltype(ret), toMatcher_t<Args>...>::doIt(matchers, storage, std::move(ret));
 	}
 
 	template<class V>
@@ -110,13 +150,13 @@ struct matcher {
 };
 
 template<size_t N>
-struct placeholder {
+struct placeholder : matcher {
 
 	template<class V>
 	using elements = int_type_map< map_entry< N, V >, nil_map >;
 
 	template<class T, class V>
-	static bool unapply_impl(T& storage, V&& value) {
+	bool unapply_impl(T& storage, V&& value) const{
 		if(!storage) storage.construct();
 		if(is_set<N>(storage)) {
 			return get<N>(storage) == std::forward<V>(value);
@@ -135,25 +175,24 @@ static placeholder<5> _6;
 static placeholder<6> _7;
 static placeholder<7> _8;
 
-struct ignore {
+struct ignore : matcher {
 
 	template<class V>
 	using elements = nil_map;
 
 	template<class T, class V>
-	static bool unapply_impl(T&, V&&) {
+	bool unapply_impl(T&, V&&) const {
 		return true;
 	}
 };
 
 static ignore _;
 
-template<class Arg0, class Arg1>
 struct example_matcher {
 
 	template<class A, class B>
-	static storage_t<match_reference<A>, match_reference<B>> unapply(std::pair<A, B>& v) {
-		storage_t<match_reference<A>, match_reference<B>> ret;
+	storage_t<A&, B&> unapply(std::pair<A, B>& v) const {
+		storage_t<A&, B&> ret;
 		ret.construct();
 		ret->set_1(v.first);
 		ret->set_2(v.second);
